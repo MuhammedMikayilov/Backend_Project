ecdd920bacbe30f5b289a9b32bbab502504e08a9
MuhammedMikayilov <mahammadsm@code.edu.az>
2020-12-30 02:56:36 +0400

    Solved bug which abouy activation

--- Backend Project/Backend Project/ViewComponents/HeaderViewComponent.cs
+++ Backend Project/Backend Project/ViewComponents/HeaderViewComponent.cs
@@ -1,4 +1,6 @@
 ﻿using Backend_Project.DAL;
+using Backend_Project.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,9 +12,11 @@ namespace Backend_Project.ViewComponents
     public class HeaderViewComponent:ViewComponent
     {
         private readonly AppDbContext _context;
-        public HeaderViewComponent(AppDbContext context)
+        private readonly UserManager<AppUser> _userManager;
+        public HeaderViewComponent(AppDbContext context, UserManager<AppUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
