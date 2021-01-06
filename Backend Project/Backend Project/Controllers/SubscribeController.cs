69d91a458b8eff25cae6f282cc4ad42fecee84fb
MuhammedMikayilov <mahammadsm@code.edu.az>
2021-01-06 11:50:52 +0400

    Comment is done

--- Backend Project/Backend Project/Controllers/SubscribeController.cs
+++ Backend Project/Backend Project/Controllers/SubscribeController.cs
@@ -1,9 +1,12 @@
 ﻿using Backend_Project.DAL;
 using Backend_Project.Models;
+using Backend_Project.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace Backend_Project.Controllers
@@ -31,6 +34,44 @@ namespace Backend_Project.Controllers
             await _context.EmailSubs.AddAsync(email);
             await _context.SaveChangesAsync();
             return RedirectToAction("Index", "Home");
+        }
+
+         public IActionResult SendMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SendMessage(CommentVM comment)
+        {
+            if (ModelState.IsValid)
+            {
+                System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient()
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential()
+                    {
+                        UserName = comment.Email,
+                        Password = comment.Password
+                    }
+                };
+                MailAddress fromEmail = new MailAddress(comment.Email, comment.Name);
+                MailAddress toEmail = new MailAddress("mikayilov.muhammed.2021@gmail.com", comment.Name);
+                MailMessage message = new MailMessage()
+                {
+                    From = fromEmail,
+                    Subject = comment.Subject,
+                    Body = comment.Message
+                };
+                message.To.Add(toEmail);
+                client.Send(message);
+            }
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
---
+++ Backend Project/Backend Project/ViewComponents/SendMessageViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Backend_Project.DAL;
+using Backend_Project.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Project.ViewComponents
+{
+    public class SendMessageViewComponent:ViewComponent
+    {
+        private readonly AppDbContext _context;
+        public SendMessageViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            CommentVM comment = new CommentVM();
+            return View(await Task.FromResult(comment));
+        }
+    }
+}
